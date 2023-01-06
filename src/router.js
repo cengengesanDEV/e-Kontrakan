b21a8f33bd51ffc1b5ce6770f3b562d34ca66edd
farisan <muhammad.farisan99@gmail.com>
2023-01-07 04:17:46 +0700

    add router home

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
   // { path: "/", element: <App />, errorElement: <Error /> },
-  { path: "/", element: <Login /> },
+  { path: "/", element: <Dashboard /> },
   { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
 ]);
 
 export default router;
