985893132f65f258c229831626c04af7bc05b26c
farisan <muhammad.farisan99@gmail.com>
2023-01-07 04:18:20 +0700

    add navigate

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,24 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import logo from "../assets/logo_white.png";
 
 import css from "../styles/component/Navbar.module.css";
 
 function Navbar() {
+  const navigate = useNavigate()
+
+  const toLogin = () => navigate('/login')
+  const toRegister = () => navigate('/register')
+  const toBeranda = () => navigate('/')
+
   return (
     <div className='container-fluid'>
       <div className={css.navbar_container}>
         <div className={css.nav_left}>
           <img src={logo} alt='Logo-kontrakan' />
-          <p>Sewa Kontrakan</p>
+          <p>House Privilege</p>
         </div>
         <div className={css.navbar_center}>
-          <li className={css.li_home}>Beranda</li>
+          <li className={css.li_home} onClick={toBeranda}>Beranda</li>
           <li className={css.li_kontrakan}>Kontrakan</li>
         </div>
         <div>
-          <button className={css.button_login}>Login</button>
-          <button className={css.button_register}>Register</button>
+          <button className={css.button_login} onClick={toLogin} >Login</button>
+          <button className={css.button_register} onClick={toRegister}>Register</button>
         </div>
       </div>
     </div>
