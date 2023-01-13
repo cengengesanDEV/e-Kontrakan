a8b89867c7c8a10dbc8db53c9ac903c223db0e0c
farisan <muhammad.farisan99@gmail.com>
2023-01-14 03:46:25 +0700

    owner design kontrakan

--- src/pages/owner/DatakontrakanOwner.js
+++ src/pages/owner/DatakontrakanOwner.js
@@ -6,11 +6,34 @@ import Sidebarowner from '../../component/Sidebar_owner'
 import ListkontrakanOwner from '../../component/ListkontrakanOwner'
 
 import css from '../../styles/page/owner/DatakontrakanOwner.module.css'
+
+import createImage from "../../assets/create.png"
+
 import { Modal } from 'react-bootstrap'
+import { SimpleDropdown } from 'react-js-dropdavn'
 
 function DatakontrakanOwner() {
 
   const [showadd, setShowadd] = useState(false)
+  const [images, setImages] = useState([]);
+
+
+  const deleteImage = (index) => {
+    setImages(images.filter((image, i) => i !== index));
+  };
+
+  const data = [
+    { label: "Aceh", value: 3 },
+    { label: "Bandung", value: 4 },
+    { label: "Bali", value: 3 },
+    { label: "DKI Jakarta", value: 1 },
+    { label: "Jawa Barat", value: 2 },
+    { label: "Jawa Tengah", value: 3 },
+    { label: "Jawa Timur", value: 3 },
+    { label: "Lampung", value: 3 },
+    { label: "Palembang", value: 3 },
+    { label: "Medan", value: 3 },
+ ];
 
 
   return (
@@ -25,7 +48,7 @@ function DatakontrakanOwner() {
               <div className={css.form_kontrakan}>
                 <div className={css.title_kontrakan}>
                   <p>Data Kontrakan</p>  
-                  <button className={css.btn_modal_2} onClick={() => setShowadd(true)}><i className="fa-solid fa-plus pe-2"></i>Add Kontrakan</button>
+                  <button className={css.btn_modal} onClick={() => setShowadd(true)}><i className="fa-solid fa-plus pe-2"></i>Add Kontrakan</button>
                 </div>
                 {/* <hr /> */}
                 <div className={css.table_bar}>
@@ -73,18 +96,66 @@ function DatakontrakanOwner() {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <div className="w-100">
-          <div className={css.form_fullname}>
+        <div className={css.container_modals}>
+          <div className={css.form_name_kontrakan}>
             <label htmlFor="">Name Kontrakan</label>
-            <input type="text" placeholder='Please input old password' />
+            <input type="text" placeholder='Please input name kontrakan' />
           </div>
           <div className={css.form_location}>
             <label htmlFor="">Location</label>
-            <input type="text" placeholder='Please input new password' />
+            <div className="w-100">
+               <SimpleDropdown
+                  options={data}
+                  labels={{
+                     notFoundSearch: "Data empty",
+                     notSelected: "Choose Location",
+                     search: "Search location",
+                     seachInputPlaceholder: "Cari Area Kontrakan",
+                  }}
+                  clearable
+                  searchable
+                  configs={{ position: { y: "center", x: "center" } }}
+               />
+            </div>
           </div>
-          <div className={css.form_location}>
-            <label htmlFor="">Detail Address</label>
-            <input type="text" placeholder='Please input confirm password' />
+          <div className={css.form_address}>
+            <label htmlFor="">Address</label>
+            <input type="text" placeholder='Please input address' />
+          </div>
+          <p className={css.choose_image}>Choose Image</p>
+          <div className={css.container_image}>
+          {images &&
+                  images.length > 0 &&
+                  images.map((image, index) => {
+                    return (
+                      <div className="position-relative">
+                          <img
+                          className={css["image-preview"]}
+                          width='120'
+                          height='120'
+                          alt=""
+                          key={index}
+                          src={URL.createObjectURL(image)}
+                        />
+                        <i onClick={() => deleteImage(index)} className={`fa-solid fa-plus bg-danger ${css.delete_image}`}></i>
+                      </div>
+                    );
+                  })}
+                {images.length < 5 && (
+                  <label for="img-product">
+                    <div className={css["add-photo"]}>
+                      <input
+                        style={{ display: "none" }}
+                        type="file"
+                        id="img-product"
+                        onChange={(e) =>
+                          setImages([...images, e.target.files[0]])
+                        }
+                      />
+                      <img src={createImage} alt="create_image" className={css.image_preview} width='120' height='120' />
+                    </div>
+                  </label>
+                )}
           </div>
           <button className={css.btn_modal_1}>Save Changes</button>
           <button className={css.btn_modal_2}>Cancel</button>
--- src/pages/owner/KontrakanLocation.js
+++ src/pages/owner/KontrakanLocation.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import css from "../../styles/page/owner/KontrakanLocation.module.css"
 import Navbar from '../../component/Navbar'
 import Footer from '../../component/Footer'
 import Sidebarowner from '../../component/Sidebar_owner'
 import ListkontrakanOwner from '../../component/ListkontrakanlocationOwner'
+import { Modal } from 'react-bootstrap'
+import createImage from "../../assets/create.png"
 
 
 function KontrakanLocation() {
+
+  const [showadd, setShowadd] = useState(false)
+  const [images, setImages] = useState([]);
+
+
+  const deleteImage = (index) => {
+    setImages(images.filter((image, i) => i !== index));
+  };
+
   return (
     <>
       <Navbar />
@@ -20,7 +31,7 @@ function KontrakanLocation() {
             <div className={css.form_kontrakan}>
                 <div className={css.title_kontrakan}>
                   <p>Data Kontrakan ~ Jakarta Utara</p>  
-                  <button><i className="fa-solid fa-plus pe-2"></i>Add Kontrakan by Location</button>
+                  <button className={css.btn_modal} onClick={() => setShowadd(true)}><i className="fa-solid fa-plus pe-2"></i>Add Kontrakan by Location</button>
                 </div>
                 {/* <hr /> */}
                 <div className={css.table_bar}>
@@ -50,6 +61,107 @@ function KontrakanLocation() {
           </div>
         </div>
       <Footer />    
+
+      {/* modal add kontrakan */}
+      <Modal
+        show={showadd}
+        onHide={() => setShowadd(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+        backdrop='static'
+        centered
+      >
+        <Modal.Header closeButton className={`text-white`} style={{backgroundColor:'#3a3a3a'}}>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Add Location
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <div className={css.container_modals}>
+          <div className={css.form_name_kontrakan}>
+            <label htmlFor="">Id kontrakan</label>
+            <input type="text" placeholder='id' disabled />
+          </div>
+          <div className={css.form_type}>
+            <label htmlFor="">Type Name Kontrakan</label>
+            <input type="text" name="" id="" placeholder='Please input type name kontrakan' />
+          </div>
+          <div className={css.form_price}>
+            <label htmlFor="">Price</label>
+            <input type="text" placeholder='Please input address' />
+          </div>
+          <div className={css.form_desc}>
+            <label htmlFor="">Description</label>
+            <input type="text" placeholder='Please input address' />
+          </div>
+          <label className={css.choose_image} htmlFor="">Fasilitas</label>
+          <div className={css.form_fasilitas}>
+            <div className={`${css.data_profile} d-flex flex-column justify-content-center gap-2`}>
+              <label htmlFor="">Ac</label>
+              <label htmlFor="">Extra Bed</label>
+              <label htmlFor="">Twin Bed</label>
+              <label htmlFor="">Toilet</label>
+              <label htmlFor="">Haduk</label>
+              <label htmlFor="">Selimut</label>
+            </div>
+            <div className={`${css.data_profile} d-flex flex-column justify-content-center gap-2`}>
+              <label htmlFor="">:</label>
+              <label htmlFor="">:</label>
+              <label htmlFor="">:</label>
+              <label htmlFor="">:</label>
+              <label htmlFor="">:</label>
+              <label htmlFor="">:</label>
+            </div>
+            <div className={`${css.data_profile} d-flex flex-column justify-content-center gap-3`}>
+              <input type="checkbox" />
+              <input type="checkbox" />
+              <input type="checkbox" />
+              <input type="checkbox" />
+              <input type="checkbox" />
+              <input type="checkbox" />
+            </div>     
+          </div>
+          <p className={css.choose_image}>Choose Image</p>
+          <div className={css.container_image}>
+          {images &&
+                  images.length > 0 &&
+                  images.map((image, index) => {
+                    return (
+                      <div className="position-relative">
+                          <img
+                          className={css["image-preview"]}
+                          width='120'
+                          height='120'
+                          alt=""
+                          key={index}
+                          src={URL.createObjectURL(image)}
+                        />
+                        <i onClick={() => deleteImage(index)} className={`fa-solid fa-plus bg-danger ${css.delete_image}`}></i>
+                      </div>
+                    );
+                  })}
+                {images.length < 5 && (
+                  <label for="img-product">
+                    <div className={css["add-photo"]}>
+                      <input
+                        style={{ display: "none" }}
+                        type="file"
+                        id="img-product"
+                        onChange={(e) =>
+                          setImages([...images, e.target.files[0]])
+                        }
+                      />
+                      <img src={createImage} alt="create_image" className={css.image_preview} width='120' height='120' />
+                    </div>
+                  </label>
+                )}
+          </div>
+          <button className={css.btn_modal_1}>Save Changes</button>
+          <button className={css.btn_modal_2}>Cancel</button>
+        </div>
+        </Modal.Body>
+      </Modal>
+
     </>
   )
 }
