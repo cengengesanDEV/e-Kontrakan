0eae6149fc09eafcb5420960239839624d6ae5c1
rivalalfalah <rivalalfalah10@gmail.com>
2023-01-26 21:02:46 +0700

    edit pass

--- src/pages/users/ProfileUser.js
+++ src/pages/users/ProfileUser.js
@@ -9,69 +9,74 @@ import { useNavigate } from "react-router-dom";
 import authAction from "../../redux/actions/auth";
 import { Modal, Spinner } from "react-bootstrap";
 import { useState } from "react";
-import { patchProfile } from "../../utils/axios";
-import { toast , ToastContainer } from "react-toastify";
+import { patchProfile, editPassword } from "../../utils/axios";
+import { toast, ToastContainer } from "react-toastify";
 
 function ProfileUser() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const profile = useSelector((state) => state.auth.profile);
 
-
   const [show, setShow] = useState(false);
   const [showpass, setShowpass] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
-  const [display, setDisplay] = useState(profile.image)
-  const [image, setImage] = useState(null)
-  const [fullname, setFullname] = useState(profile.fullname)
-  const [address, setAddress] = useState(profile.address)
-  const [gender, setGender] = useState(profile.gender)
-  const [location, setLocation] = useState(profile.location)
-  const [loading, setLoading] = useState(false)
-  const [newPass, setNewPass] = useState('')
-  const [oldPass, setOldPass] = useState('')
-  const [confirmPass, setConfimPass] = useState('')
-
+  const [display, setDisplay] = useState(profile.image);
+  const [image, setImage] = useState(null);
+  const [fullname, setFullname] = useState(profile.fullname);
+  const [address, setAddress] = useState(profile.address);
+  const [gender, setGender] = useState(profile.gender);
+  const [location, setLocation] = useState(profile.location);
+  const [loading, setLoading] = useState(false);
+  const [newPass, setNewPass] = useState("");
+  const [oldPass, setOldPass] = useState("");
+  const [confirmPass, setConfimPass] = useState("");
 
   const handleImagePreview = (e) => {
-    setImage(e.target.files[0])
+    setImage(e.target.files[0]);
     // console.log(typeof(e.target.files[0]))
     // console.log(e.target.files)
-    setDisplay(URL.createObjectURL(e.target.files[0]))
-  }
+    setDisplay(URL.createObjectURL(e.target.files[0]));
+  };
 
-  const valueFullname = (e) => {setFullname(e.target.value)}
-  const valueAddress = (e) => {setAddress(e.target.value)}
-  const valueGender = (e) => {setGender(e.target.value)}
-  const valueLocation = (e) => {setLocation(e.target.value)}
+  const valueFullname = (e) => {
+    setFullname(e.target.value);
+  };
+  const valueAddress = (e) => {
+    setAddress(e.target.value);
+  };
+  const valueGender = (e) => {
+    setGender(e.target.value);
+  };
+  const valueLocation = (e) => {
+    setLocation(e.target.value);
+  };
 
   const handleEditProfile = async () => {
     try {
-      setLoading(true)
-      const getToken = localStorage.getItem('token')      
-      const formData = new FormData()
-      if(fullname) formData.append('full_name', fullname)
-      if(address) formData.append('address', address)
-      if(location) formData.append('location', location)
-      if(gender) formData.append('gender', gender)
-      if(image) formData.append('image', image)
-      const result = await patchProfile(formData, getToken)
-      await dispatch(authAction.profileThunk(getToken))
+      setLoading(true);
+      const getToken = localStorage.getItem("token");
+      const formData = new FormData();
+      if (fullname) formData.append("full_name", fullname);
+      if (address) formData.append("address", address);
+      if (location) formData.append("location", location);
+      if (gender) formData.append("gender", gender);
+      if (image) formData.append("image", image);
+      const result = await patchProfile(formData, getToken);
+      await dispatch(authAction.profileThunk(getToken));
       toast.success(result.data.msg, {
         position: toast.POSITION.TOP_RIGHT,
-      })
-      setShowEdit(false)
-      setLoading(false)
+      });
+      setShowEdit(false);
+      setLoading(false);
     } catch (err) {
       // console.log(err)
-      setShow(false)
-      setLoading(false)
+      setShow(false);
+      setLoading(false);
       toast.error(err.response.data.msg, {
         position: toast.POSITION.TOP_RIGHT,
-      })
+      });
     }
-  }
-  
+  };
 
   const handleLogout = () => {
     const getToken = localStorage.getItem("token");
@@ -82,14 +87,45 @@ function ProfileUser() {
     );
   };
 
+  const handleEditPass = async () => {
+    try {
+      if (!oldPass || !newPass || !confirmPass) {
+        return toast.error("please input all form to change password", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+      const token = await localStorage.getItem("token");
+      await editPassword(token, {
+        oldpass: oldPass,
+        newpass: newPass,
+        confirmpass: confirmPass,
+      });
+      toast.success("edit password success", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      setShowpass(false);
+      setNewPass("");
+      setOldPass("");
+      setConfimPass("");
+    } catch (error) {
+      toast.error(error.response.data.msg, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
   return (
     <>
-    <ToastContainer />
+      <ToastContainer />
       <Navbar />
       <main className={css.container}>
         <div className={css.button_container}>
-          <button className={css.btn_modal_1} onClick={()=> setShowpass(true)}>Edit password</button>
-          <button className={css.btn_modal_1} onClick={()=> setShowEdit(true)}>Edit profile</button>
+          <button className={css.btn_modal_1} onClick={() => setShowpass(true)}>
+            Edit password
+          </button>
+          <button className={css.btn_modal_1} onClick={() => setShowEdit(true)}>
+            Edit profile
+          </button>
         </div>
         <side className={css.profile_container}>
           <span className={css.profile_Top}>
@@ -149,7 +185,7 @@ function ProfileUser() {
           </span>
         </side>
         <div className="d-flex justify-content-end mb-5">
-          <button className={css.logout} onClick={()=> setShow(true)}>
+          <button className={css.logout} onClick={() => setShow(true)}>
             logout
           </button>
         </div>
@@ -175,8 +211,12 @@ function ProfileUser() {
         </Modal.Header>
         <Modal.Body>
           <div className="w-100">
-            <button className={css.btn_modal_1} onClick={handleLogout}>yes</button>
-            <button className={css.btn_modal_2} onClick={()=> setShow(false)}>Cancel</button>
+            <button className={css.btn_modal_1} onClick={handleLogout}>
+              yes
+            </button>
+            <button className={css.btn_modal_2} onClick={() => setShow(false)}>
+              Cancel
+            </button>
           </div>
         </Modal.Body>
       </Modal>
@@ -186,90 +226,191 @@ function ProfileUser() {
         onHide={() => setShowEdit(false)}
         dialogClassName="modal-90w"
         aria-labelledby="example-custom-modal-styling-title"
-        backdrop='static'
+        backdrop="static"
         centered
       >
-        <Modal.Header closeButton className={`text-white`} style={{backgroundColor:'#3a3a3a'}}>
+        <Modal.Header
+          closeButton
+          className={`text-white`}
+          style={{ backgroundColor: "#3a3a3a" }}
+        >
           <Modal.Title id="example-custom-modal-styling-title">
             Profile Edit
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <div className={css.scroll}>
-          <label htmlFor="" className={css.title_foto_profile}>Foto profile</label>
-          <div className={css.form_fullname}>
-            <label htmlFor="img_picture"><img src={display} alt="empty-img" className={css.image_modal_preview} /></label>
-            <input type="file" id="img_picture"className='d-none' onChange={handleImagePreview} />
-          </div>
-          <div className={css.form_fullname}>
-            <label htmlFor="">Fullname</label>
-            <input type="text" value={fullname} name="full_name" id="" placeholder='Please input Fullname' onChange={valueFullname} />
-          </div>
-          <div className={css.radio_gender}>
-            <p className={css.gender_title}>Gender</p>
-            <div className="py-3">
-              <div className="form-check">
-                <input className="form-check-input" value='male' type="radio" name="gender" id="flexRadioDefault1" onChange={valueGender} checked={gender === 'male' ? true : false} />
-                <label className="form-check-label" for="flexRadioDefault1">
-                  Male
-                </label>
-              </div>
-              <div className="form-check pt-2">
-                <input className="form-check-input" type="radio" name="gender" id="flexRadioDefault2" onChange={valueGender} value='female' checked={gender === 'female' ? true : false} />
-                <label className="form-check-label" for="flexRadioDefault2">
-                  Female
-                </label>
+          <div className={css.scroll}>
+            <label htmlFor="" className={css.title_foto_profile}>
+              Foto profile
+            </label>
+            <div className={css.form_fullname}>
+              <label htmlFor="img_picture">
+                <img
+                  src={display}
+                  alt="empty-img"
+                  className={css.image_modal_preview}
+                />
+              </label>
+              <input
+                type="file"
+                id="img_picture"
+                className="d-none"
+                onChange={handleImagePreview}
+              />
+            </div>
+            <div className={css.form_fullname}>
+              <label htmlFor="">Fullname</label>
+              <input
+                type="text"
+                value={fullname}
+                name="full_name"
+                id=""
+                placeholder="Please input Fullname"
+                onChange={valueFullname}
+              />
+            </div>
+            <div className={css.radio_gender}>
+              <p className={css.gender_title}>Gender</p>
+              <div className="py-3">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    value="male"
+                    type="radio"
+                    name="gender"
+                    id="flexRadioDefault1"
+                    onChange={valueGender}
+                    checked={gender === "male" ? true : false}
+                  />
+                  <label className="form-check-label" for="flexRadioDefault1">
+                    Male
+                  </label>
+                </div>
+                <div className="form-check pt-2">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="gender"
+                    id="flexRadioDefault2"
+                    onChange={valueGender}
+                    value="female"
+                    checked={gender === "female" ? true : false}
+                  />
+                  <label className="form-check-label" for="flexRadioDefault2">
+                    Female
+                  </label>
+                </div>
               </div>
             </div>
+            <div className={css.form_address}>
+              <label htmlFor="">Address</label>
+              <input
+                type="text"
+                name="address"
+                value={address}
+                onChange={valueAddress}
+                placeholder="Please input address"
+              />
+            </div>
+            <div className={css.form_location}>
+              <label htmlFor="">Location</label>
+              <input
+                type="text"
+                value={location}
+                name="location"
+                onChange={valueLocation}
+                placeholder="Please input Location"
+              />
+            </div>
+            {loading ? (
+              <div className="d-flex justify-content-center align-items-center py-4">
+                <Spinner animation="border" />{" "}
+              </div>
+            ) : (
+              <>
+                <button className={css.btn_modal_1} onClick={handleEditProfile}>
+                  Save Changes
+                </button>
+                <button
+                  className={css.btn_modal_2}
+                  onClick={() => setShowEdit(false)}
+                >
+                  Cancel
+                </button>{" "}
+              </>
+            )}
           </div>
-          <div className={css.form_address}>
-            <label htmlFor="">Address</label>
-            <input type="text" name="address" value={address} onChange={valueAddress} placeholder='Please input address' />
-          </div>
-          <div className={css.form_location}>
-            <label htmlFor="">Location</label>
-            <input type="text" value={location} name="location" onChange={valueLocation}  placeholder='Please input Location' />
-          </div>
-          {loading ? <div className="d-flex justify-content-center align-items-center py-4">
-                    <Spinner animation="border" /> </div> : <><button className={css.btn_modal_1} onClick={handleEditProfile}>Save Changes</button> 
-          <button className={css.btn_modal_2} onClick={() => setShowEdit(false)}>Cancel</button> </>}
-        </div>
         </Modal.Body>
       </Modal>
 
       <Modal
         show={showpass}
-        onHide={() => setShowpass(false)}
+        onHide={() => {
+          setNewPass("");
+          setOldPass("");
+          setConfimPass("");
+          setShowpass(false);
+        }}
         dialogClassName="modal-90w"
         aria-labelledby="example-custom-modal-styling-title"
-        backdrop='static'
+        backdrop="static"
         centered
       >
-        <Modal.Header closeButton className={`text-white`} style={{backgroundColor:'#3a3a3a'}}>
+        <Modal.Header
+          closeButton
+          className={`text-white`}
+          style={{ backgroundColor: "#3a3a3a" }}
+        >
           <Modal.Title id="example-custom-modal-styling-title">
             Change Password
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <div className="w-100">
-          <div className={css.form_fullname}>
-            <label htmlFor="">Old Password</label>
-            <input type="text" placeholder='Please input old password' value={oldPass} onChange={(e) => setOldPass(e.target.value)}/>
+          <div className="w-100">
+            <div className={css.form_fullname}>
+              <label htmlFor="">Old Password</label>
+              <input
+                type="text"
+                placeholder="Please input old password"
+                value={oldPass}
+                onChange={(e) => setOldPass(e.target.value)}
+              />
+            </div>
+            <div className={css.form_location}>
+              <label htmlFor="">New Password</label>
+              <input
+                type="text"
+                placeholder="Please input new password"
+                value={newPass}
+                onChange={(e) => setNewPass(e.target.value)}
+              />
+            </div>
+            <div className={css.form_location}>
+              <label htmlFor="">Confirm Password</label>
+              <input
+                type="text"
+                placeholder="Please input confirm password"
+                value={confirmPass}
+                onChange={(e) => setConfimPass(e.target.value)}
+              />
+            </div>
+            <button className={css.btn_modal_1} onClick={handleEditPass}>
+              Save Changes
+            </button>
+            <button
+              className={css.btn_modal_2}
+              onClick={() => {
+                setNewPass("");
+                setOldPass("");
+                setConfimPass("");
+                setShowpass(false);
+              }}
+            >
+              Cancel
+            </button>
           </div>
-          <div className={css.form_location}>
-            <label htmlFor="">New Password</label>
-            <input type="text" placeholder='Please input new password' value={newPass} onChange={(e) => setNewPass(e.target.value)}/>
-          </div>
-          <div className={css.form_location}>
-            <label htmlFor="">Confirm Password</label>
-            <input type="text" placeholder='Please input confirm password' value={confirmPass} onChange={(e) => setConfimPass(e.target.value)}/>
-          </div>
-          <button className={css.btn_modal_1}>Save Changes</button>
-          <button className={css.btn_modal_2}>Cancel</button>
-        </div>
         </Modal.Body>
       </Modal>
-
     </>
   );
 }
